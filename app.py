0691e47c85856cb60591b97134f1fcb11e003ae4
root <root@LENOVO-IVAN.localdomain>
2022-12-03 17:58:26 +0100

    Add model changes and migrations, make sure migrations run before app starts.

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_smorest import Api
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from blocklist import BLOCKLIST
+from dotenv import load_dotenv
+import os
 
 from db import db
 
@@ -17,13 +19,15 @@ from resources.tag import blp as TagBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
+    
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
--- migrations/env.py
+++ migrations/env.py
@@ -52,7 +52,8 @@ def run_migrations_offline():
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url, target_metadata=get_metadata(), literal_binds=True
+        url=url, target_metadata=get_metadata(), 
+        literal_binds=True
     )
 
     with context.begin_transaction():
@@ -81,8 +82,8 @@ def run_migrations_online():
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection,
-            target_metadata=get_metadata(),
+            connection=connection,  
+            target_metadata=get_metadata(), 
             process_revision_directives=process_revision_directives,
             **current_app.extensions['migrate'].configure_args
         )
